02d787b8593f83ba2a86eb07bdce3a518faf51b3
julesfredo <salmonjulian5@gmail.com>
2025-12-05 12:52:48 -0500

    API call to arctic, artwork 1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-index.js
+async function fetchCaucusYear() {
+    try{
+        const response = await fetch(`https://api.artic.edu/api/v1/artworks/151545`);
+        if(!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        var yearArtwork1 = data.data.date_end;
+        return yearArtwork1;
+        
+    } catch(error) {
+        console.log(response.status, error);
+    }
+}
+
+function fetchArtworkYear() {
+    fetchCaucusYear();
+}
