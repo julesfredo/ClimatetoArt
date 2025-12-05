65d29e0ed61b6ad476e11a5b6fff6042b4697bd7
julesfredo <salmonjulian5@gmail.com>
2025-12-05 18:35:55 -0500

    Add API result to html - Artwork 2

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
-async function fetchCaucusYear() {
+let japaneseImpressionism = document.getElementById("japaneseImpressionismYear");
+let chicago = document.getElementById("chicagoYear");
+let hope = document.getElementById("hopeYear");
+
+async function fetchJapaneseImpressionismYear() {
     try{
         const response = await fetch(`https://api.artic.edu/api/v1/artworks/151545`);
         if(!response.ok) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        var yearArtwork1 = data.data.date_end;
-        return yearArtwork1;
+        let japaneseImpressionismYear = data.data.date_end;
+        return japaneseImpressionismYear;
+        
+    } catch(error) {
+        console.log(response.status, error);
+    }
+}
+async function fetchChicagoYear() {
+    try{
+        const response = await fetch(`https://api.artic.edu/api/v1/artworks/53474`);
+        if(!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        let chicagoData = data.data.date_end;
+        return chicagoData;
+        
+    } catch(error) {
+        console.log(response.status, error);
+    }
+}
+async function fetchHopeYear() {
+    try{
+        const response = await fetch(`https://api.artic.edu/api/v1/artworks/229396`);
+        if(!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        let hopeData = data.data.date_end;
+        return hopeData;
         
     } catch(error) {
         console.log(response.status, error);
     }
 }
 
-function fetchArtworkYear() {
-    fetchCaucusYear();
+async function fetchArtworkYear() {
+    japaneseImpressionism.innerHTML = await fetchJapaneseImpressionismYear();
 }
