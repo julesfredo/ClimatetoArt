4030c95d31c86daa47c3a63a53efd06b4fc42b30
julesfredo <salmonjulian5@gmail.com>
2025-12-07 13:18:18 -0500

    MeteoAPI call processed by greedy algorithm, result implemented in Weather.html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let year;
-let weatherAPIUrl = 'https://archive-api.open-meteo.com/v1/archive?latitude=41.85&longitude=-87.65&start_date='
+let weatherAPIUrl = 'https://archive-api.open-meteo.com/v1/archive?latitude=41.85&longitude=-87.65&start_date=';
 let weatherDate = '-06-20&end_date=';
-let weatherAPIValues = '-09-22&daily=apparent_temperature_max,apparent_temperature_min&temperature_unit=fahrenheit';
+let weatherAPIValues = '-09-22&daily=apparent_temperature_max,apparent_temperature_min&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch';
 
 let articAPIUrl = 'https://api.artic.edu/api/v1/artworks/';
 
@@ -13,20 +13,22 @@ let japaneseImpressionism = document.getElementById("japaneseImpressionismYear")
 let chicago = document.getElementById("chicagoYear");
 let hope = document.getElementById("hopeYear");
 
+let maxTempId = document.getElementById("maxTemp");
+let minTempId = document.getElementById("minTemp");
 
 const corsHeader = new Headers();
 corsHeader.append("Access-Control-Allow-Origin", "*");
 
 async function fetchJapaneseImpressionismYear() {
     try{
-        const response = await fetch(articAPIUrl + artwork1);
+        let apiUrl = articAPIUrl + artwork1;
+        const response = await fetch(apiUrl);
         if(!response.ok) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
         let japaneseImpressionismYear = data.data.date_end;
         return japaneseImpressionismYear;
-        
     } catch(error) {
         console.log(response.status, error);
     }
@@ -34,22 +36,24 @@ async function fetchJapaneseImpressionismYear() {
 
 async function fetchChicagoYear() {
     try{
-        const response = await fetch(articAPIUrl + artwork2);
+        let apiUrl = articAPIUrl + artwork2;
+        const response = await fetch(apiUrl);
         if(!response.ok) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
         let chicagoData = data.data.date_end;
         return chicagoData;
-        
     } catch(error) {
         console.log(response.status, error);
     }
 }
 
 async function fetchHopeYear() {
+    
     try{
-        const response = await fetch(articAPIUrl + artwork3);
+        let apiUrl = articAPIUrl + artwork3;
+        const response = await fetch(apiUrl);
         if(!response.ok) {
             throw new Error('Network response was not ok');
         }
@@ -72,11 +76,26 @@ function setYear(y) {
     sessionStorage.setItem("year", y);
 }
 
+function findMaxMin(maxTemps, minTemps) {
+    let maxTemp = -Infinity;
+    let minTemp = Infinity;
+
+    for (let i = 0; i < maxTemps.length; i++) {
+        if (maxTemps[i] > maxTemp) {
+            maxTemp = maxTemps[i];
+        }
+        if (minTemps[i] < minTemp) {
+            minTemp = minTemps[i];
+        }
+    }
+    return [maxTemp, minTemp];
+}
+
 async function fetchTemps() {
     try {
     year = sessionStorage.getItem("year");
     apiUrl = weatherAPIUrl + year + weatherDate + year + weatherAPIValues;
-    const response = await fetch(weatherAPIUrl, {
+    const response = await fetch(apiUrl, {
         method: 'GET',
         headers: corsHeader
     });
@@ -85,6 +104,13 @@ async function fetchTemps() {
         throw new Error('Network response was not ok');
     }
     const data = await response.json();
+    let maxTemps = data.daily.apparent_temperature_max;
+    let minTemps = data.daily.apparent_temperature_min;
+    
+    let temps = findMaxMin(maxTemps, minTemps);
+    maxTempId.innerHTML = temps[0];
+    minTempId.innerHTML = temps[1];
+
     console.log(data);
 } catch (error) {
         console.log("Error ", error);
